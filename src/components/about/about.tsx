786b096d0fdbcaf6950796acfd75992a3ca8b1f4
Ali <ali.abou-zahr@zingermans.com>
2025-06-01 15:03:45 -0400

    initial commit

---
+++ src/components/about/about.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './about.css'
+// import myImage from '../../assets/my-image2.jpg'
+// import {GiGraduateCap} from 'react-icons/gi'
+// import {BsBookmarkStar} from 'react-icons/bs'
+// import {TfiWorld} from 'react-icons/tfi'
+
+function About() {
+  return (
+    <section id='about'>
+      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+            <p>I'm a Fullstack Developer with a strong emphasis on frontend technologies</p>
+      </div>
+    </section>
+  )
+}
+
+
+export { About };
