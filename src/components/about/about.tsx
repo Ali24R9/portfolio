d32f27c6921bf45081b5e50951b3bd14816e4d3c
Ali <ali.abou-zahr@zingermans.com>
2025-06-03 13:02:39 -0400

    styling and customizing components

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -12,7 +12,15 @@ function About() {
       <h2>About Myself</h2>
 
       <div className="container about__container">
-            <p>I'm a Fullstack Developer with a strong emphasis on frontend technologies</p>
+            <p>Hi, I’m Ali — a former Chemistry grad turned full-stack software engineer. My path into tech wasn’t traditional, but it’s been driven by a deep curiosity, hands-on learning, and mentorship from great engineers along the way. I began my development career building and shipping production code for both startups and enterprise teams, where I gained foundational experience across the stack.</p>
+
+<p>After relocating to a new city, I took a detour and opened a restaurant — an experience that sharpened my business instincts and taught me the value of understanding user needs from the ground up. When COVID hit, I returned to my true passion: software development. I re-entered the field at Zingerman’s in Ann Arbor, where I led full-stack development across 12+ websites. I built modern CMS experiences using Sage by Roots (making Wordpress fun and modern for developers), deployed code through CI/CD pipelines, and worked directly on Linux servers to troubleshoot production issues. It was a true end-to-end engineering role that let me merge technical depth with business awareness.</p>
+
+<p>Today, I’m thrilled to be back in tech, constantly building, learning, and pushing myself forward. Whether it’s crafting responsive UIs, deploying robust APIs, or diving into DevOps, I love solving real problems with thoughtful code.</p>
+
+<p>Outside of coding, I’m a movie nerd who can talk Star Wars, sports, and storytelling for hours. If you’re looking for a developer who brings technical skill, business perspective, and a collaborative mindset — I’d love to connect.
+
+</p>
       </div>
     </section>
   )
--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -1,13 +1,162 @@
 import React from 'react'
-import './works.css'
+import './works.scss'
 import IMG1 from '../../assets/img1.png'
 import IMG2 from '../../assets/img1.png'
 import IMG3 from '../../assets/img1.png'
 import IMG4 from '../../assets/img1.png'
 
+if(document.body.classList.contains('home')) {
+
+  //homepage hero functionality
+  let shop = document.getElementById("hero-wrapper-shop");
+  let eat = document.getElementById("hero-wrapper-eat");
+  let celebrate = document.getElementById("hero-wrapper-celebrate");
+  let learn = document.getElementById("hero-wrapper-learn");
+
+  let shopShop = document.getElementById("shop-shop");
+  let shopEat = document.getElementById("shop-eat");
+  let shopCelebrate = document.getElementById("shop-celebrate");
+  let shopLearn = document.getElementById("shop-learn");
+
+  let eatShop = document.getElementById("eat-shop");
+  let eatEat = document.getElementById("eat-eat");
+  let eatCelebrate = document.getElementById("eat-celebrate");
+  let eatLearn = document.getElementById("eat-learn");
+
+  let celebrateShop = document.getElementById("celebrate-shop");
+  let celebrateEat = document.getElementById("celebrate-eat");
+  let celebrateCelebrate = document.getElementById("celebrate-celebrate");
+  let celebrateLearn = document.getElementById("celebrate-learn");
+
+  let learnShop = document.getElementById("learn-shop");
+  let learnEat = document.getElementById("learn-eat");
+  let learnCelebrate = document.getElementById("learn-celebrate");
+  let learnLearn = document.getElementById("learn-learn");
+
+  //shop display
+  shopShop.addEventListener("click", function(){
+    shop.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  shopEat.addEventListener("click", function(){
+    eat.style.display = 'block';
+
+    shop.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  shopCelebrate.addEventListener("click", function(){
+    celebrate.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    shop.style.display = 'none';
+  }, false);
+  shopLearn.addEventListener("click", function(){
+    learn.style.display = 'block';
+
+    eat.style.display = 'none';
+    shop.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+
+  //eat display
+  eatShop.addEventListener("click", function(){
+    shop.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  eatEat.addEventListener("click", function(){
+    eat.style.display = 'block';
+
+    shop.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  eatCelebrate.addEventListener("click", function(){
+    celebrate.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    shop.style.display = 'none';
+  }, false);
+  eatLearn.addEventListener("click", function(){
+    learn.style.display = 'block';
+
+    eat.style.display = 'none';
+    shop.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+
+  //celebrate display
+  celebrateShop.addEventListener("click", function(){
+    shop.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  celebrateEat.addEventListener("click", function(){
+    eat.style.display = 'block';
+
+    shop.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  celebrateCelebrate.addEventListener("click", function(){
+    celebrate.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    shop.style.display = 'none';
+  }, false);
+  celebrateLearn.addEventListener("click", function(){
+    learn.style.display = 'block';
+
+    eat.style.display = 'none';
+    shop.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+
+  //learn display
+  learnShop.addEventListener("click", function(){
+    shop.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  learnEat.addEventListener("click", function(){
+    eat.style.display = 'block';
+
+    shop.style.display = 'none';
+    learn.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+  learnCelebrate.addEventListener("click", function(){
+    celebrate.style.display = 'block';
+
+    eat.style.display = 'none';
+    learn.style.display = 'none';
+    shop.style.display = 'none';
+  }, false);
+  learnLearn.addEventListener("click", function(){
+    learn.style.display = 'block';
+
+    eat.style.display = 'none';
+    shop.style.display = 'none';
+    celebrate.style.display = 'none';
+  }, false);
+}
+
 const Works = () => {
   return (
-    <section>
+    <section id="works">
 
       <h5>My Recent Works & Contributions</h5>
       <h2>Projects</h2>
@@ -18,7 +167,7 @@ const Works = () => {
             <div className="portfolio__item-image">
                 <img src={IMG1} alt="" />
             </div>
-            <h3>Fashion Gallery</h3>
+            <h3>Landing Page (live site)</h3>
             <small className='text-light'>Spring Boot | ReactJs | MySQL</small>
             <div className="portfolio__item-cta">
               <a href="https://www.zingermanscommunity.com" target="_blank" rel='noreferrer' className='btn'>Github</a>
